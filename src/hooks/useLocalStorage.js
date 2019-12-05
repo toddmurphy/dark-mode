fdd16fcffc202e4d331c4516633a173f54f648aa
toddmurphy <toddmurphy93@gmail.com>
2019-12-04 23:16:58 -0400

    removed imports not being used

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const UseLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
